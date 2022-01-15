a296409ba3b1037711b3a9a41cc79f94a0e4db10
YumYumNyang <khw121699@gmail.com>
2022-01-15 11:37:32 +0900

    feat: styled-component setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+`;
+
+const theme = {
+  brandColor: {
+    orange: '#F45400'
+  },
+  grayScale: {
+    black: '#000000',
+    body: '#666666',
+    sub: '#999999',
+    disable: '#cccccc',
+    border: '#e1e1e1',
+    searchBar: '#f3f3f3',
+    background: '#f7f7f7',
+    white: '#ffffff'
+  }
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
